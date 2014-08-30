46b6f5e7d2183a2505ca3fa68b35f9cb970d83d1
Taylor Allred <2000ponyz@gmail.com>
2014-08-30 10:48:31 -0600

    Updated table, still broken

--- app/scripts/controllers/table2.js
+++ app/scripts/controllers/table2.js
@@ -10,47 +10,10 @@ angular
     .module('gasNgoApp')
     .controller('table2Ctrl', function($scope, $filter, $http) {
     $scope.users = [
-        {id: 1, name: 'awesome user1', status: 2, group: 4, groupName: 'admin'},
-        {id: 2, name: 'awesome user2', status: undefined, group: 3, groupName: 'vip'},
-        {id: 3, name: 'awesome user3', status: 2, group: null}
+        {id: 1, name: 'awesome user1'},
+        {id: 2, name: 'awesome user2'},
+        {id: 3, name: 'awesome user3'}
     ];
-
-    $scope.statuses = [
-        {value: 1, text: 'status1'},
-        {value: 2, text: 'status2'},
-        {value: 3, text: 'status3'},
-        {value: 4, text: 'status4'}
-    ];
-
-    $scope.groups = [];
-    $scope.loadGroups = function() {
-        return $scope.groups.length ? null : $http.get('/groups').success(function(data) {
-            $scope.groups = data;
-        });
-    };
-
-    $scope.showGroup = function(user) {
-        if(user.group && $scope.groups.length) {
-            var selected = $filter('filter')($scope.groups, {id: user.group});
-            return selected.length ? selected[0].text : 'Not set';
-        } else {
-            return user.groupName || 'Not set';
-        }
-    };
-
-    $scope.showStatus = function(user) {
-        var selected = [];
-        if(user.status) {
-            selected = $filter('filter')($scope.statuses, {value: user.status});
-        }
-        return selected.length ? selected[0].text : 'Not set';
-    };
-
-    $scope.checkName = function(data, id) {
-        if (id === 2 && data !== 'awesome') {
-            return "Username 2 should be `awesome`";
-        }
-    };
 
     $scope.saveUser = function(data, id) {
         //$scope.user not updated yet
@@ -67,9 +30,7 @@ angular
     $scope.addUser = function() {
         $scope.inserted = {
             id: $scope.users.length+1,
-            name: '',
-            status: null,
-            group: null
+            name: ''
         };
         $scope.users.push($scope.inserted);
     };
